36c5fd1c24f564e45537779ee6b2448e321b93bb
Weicheng Yuan <yuan0089@outlook.com>
2025-04-30 04:43:22 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,25 +6,14 @@ function App() {
 
   return (
     <>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
       <h1>YUANWEICHENG</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-           {count}
-        </button>
         <p>
-          袁炜程袁炜程袁炜程袁炜程袁炜程袁炜程袁炜程袁炜程袁炜程
+          袁炜程
         </p>
       </div>
       <p className="read-the-docs">
-        ？？？？？？？
+        yuanweicheng
       </p>
     </>
   )
